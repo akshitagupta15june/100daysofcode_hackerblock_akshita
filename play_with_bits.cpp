045805ca3f1d85c808c016205c1a597ef12a0101
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-05-17 13:17:42 +0530

    Create play_with_bits.cpp

---
+++ play_with_bits.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define ll long long
+ll playwithbits(ll a){
+    int ans = 0;
+    for(;a>0; a= a>>1)
+        ans += a&1;
+    return ans;
+}
+int main() {
+    ll q,a,b,ans;
+    cin>>q;
+    while(q!=0){
+        cin>>a>>b;
+        ans = 0;
+        for(ll i = a; i<=b; i++)
+         ans += playwithbits(i);
+        cout<<ans<<endl;
+        q--;
+    }
+}
