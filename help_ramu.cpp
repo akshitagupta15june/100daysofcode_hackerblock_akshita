d4bf5141c1f5cb8bf69ea830953b780fcdcb989d
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-06-02 16:40:37 +0530

    Create help_ramu.cpp

---
+++ help_ramu.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int main(int argc, char **argv)
+{
+	int t;
+    cin>>t;
+    int c1,c2,c3,c4,n,m;
+    int rick[1005],cab[1005];
+    while(t--)
+    {
+        
+        cin>>c1>>c2>>c3>>c4;
+        cin>>n>>m;
+        for(int i=0;i<n;i++)
+        {
+            cin>>rick[i];
+        }
+        for(int i=0;i<m;i++)
+        {
+            cin>>cab[i];
+        }
+        int rickcost=0;
+        for(int i=0;i<n;i++)
+        {
+            rickcost+=min(c1*rick[i],c2);
+        }
+        rickcost=min(rickcost,c3);
+        int cabcost=0;
+        for(int i=0;i<m;i++)
+        {
+            cabcost+=min(c1*cab[i],c2);
+        }
+        cabcost=min(cabcost,c3);
+        int finalcost=0;
+        finalcost=min(c4,cabcost+rickcost);
+        cout<<finalcost<<endl;
+    }
+	return 0;
+}
