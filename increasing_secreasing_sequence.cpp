c58052c34067d72cf57695205681e9a27a54f1a4
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-06-10 11:33:50 +0530

    Create increasing_secreasing_sequence.cpp

---
+++ increasing_secreasing_sequence.cpp
@@ -0,0 +1,30 @@
+int main() {
+    int n;
+    cin>>n;
+    int e,p,inc=0;
+    cin>>p;
+    n--;
+    while(n>0){
+        cin>>e;
+        if(e==p){
+            cout<<"false";
+            return 0;
+        }
+        if(e<p && inc==0){
+            n--;
+            p=e;
+            continue;
+        }
+        else if(e<p && inc==1){
+            cout<<"false";
+            return 0;
+        }
+        else if(e>p){
+            inc=1;
+        }
+        p=e;
+        n--;
+    }
+    cout<<"true";
+	return 0;
+}
