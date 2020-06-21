0a579404c5744da0d3e4010cb5e6bcddd9248415
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-06-21 20:40:56 +0530

    Update teams.cpp

--- teams.cpp
+++ teams.cpp
@@ -1,32 +1,30 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int maximum(int n,int m)
+long long int maximum(long long int n,long long int m)
 {
-    int x=n-(m-1);
-    int maxi=(x*(x-1))/2;
-    return maxi;
+	long long int x=n-(m-1);
+	long long int maxi=(x*(x-1))/2;
+	return maxi;
 }
-int minimum(int n,int m)
+long long int minimum(long long int n,long long int m)
 {
-    int y=0;
-    int p=0;
-    int x=n/m;
-    if(n%m!=0)
-    {
-        y=x+1;
-         p=n%m;
-    }
-    int mini=(y*(y-1)/2)*p+(m-p)*(x*(x-1)/2);
-    return mini;
+	long long int y=0;
+	long long int p=0;
+    long long int x=n/m;
+	if(n%m!=0)
+	{
+		y=x+1;
+		p=n%m;
+
+	}
+	long long int mini=(y*(y-1)/2)*p+(m-p)*(x*(x-1)/2);
+	return mini;
 }
-int main(int argc, char **argv)
+int main()
 {
-	int n,m;
-    cin>>n>>m;
-    cout<<minimum(n,m)<<" ";
-    cout<<maximum(n,m);
-    
-    
-    
+	long long int n,m;
+	cin>>n>>m;
+	cout<<minimum(n,m)<<" ";
+	cout<<maximum(n,m);
 	return 0;
 }
