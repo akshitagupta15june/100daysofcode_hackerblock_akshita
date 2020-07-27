2d09465d466029a8404aa3ffb75cc41616957fa0
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-07-27 16:10:20 +0530

    Create findingCB_numbers.cpp

---
+++ findingCB_numbers.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int fact(long long a)
+{
+    if(a<=1)
+        return 0;
+    if(a<=3)
+        return 1;    
+    if(a%3==0||a%2==0)
+        return 0;
+    for(int i=5;i*i<a;i=i+6)
+        if(a%(i)==0||a%(i+2)==0)
+            return 0;
+    return 1;            
+}
+
+bool isvisited(int *v,int i,int j){
+    for(;i<=j;i++)
+        if(v[i]==1)
+            //Already visited
+            return true;
+    //If all goes well, then not visited, we can count for this CB number
+    return false;
+}
+int main() {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int count=0;
+    //Array to keep track of visisted indeces
+    int v[n]={0};
+    //Outter loop for size of the subaaray/substring
+    for(int i=0;i<n;i++)
+        //To create subarrays
+        for(int j=0;j<n-i;j++)
+        {
+            long long num=0;
+            //Start from j
+            int temp=j;
+            //Go upto j+i
+            while(temp<=j+i)
+            {
+                num=num*10+(s[temp]-'0');
+                temp++;
+            }
+            //If a CB number
+            if(fact(num)==1)
+                {
+                    //Now, check if indexes are not already used is any other CB number detected before.
+                    if(!isvisited(v,j,j+i)){
+                    //If you want to see the CB numbers detected, uncomment the following statement
+                    // cout<<num<<endl;
+                        count++;
+                        //Mark the detected indexes as visited
+                        for(int k=j;k<=j+i;k++)
+                            v[k]=1;
+                    }
+                }
+
+        }
+        cout<<count;
+	return 0;
+}
