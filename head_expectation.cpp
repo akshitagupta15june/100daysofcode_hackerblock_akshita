2a21501cf37885f7ef70703d0a806d893b728bf9
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-08-05 14:10:31 +0530

    Create head_expectation.cpp

---
+++ head_expectation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+int main() {
+	long long int n;
+	cin>>n;
+	long long int x;
+	x=pow(2,n+1)-2;
+	cout<<x;
+	return 0;
+}
